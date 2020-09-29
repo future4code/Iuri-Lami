8c022a79d345a526928d605f47170202440cf0d0
IuriLami <iuri.lami@gmail.com>
2020-09-29 16:58:32 -0300

    Exercicios Aula 10

---
+++ semana3/operadores/src/index.js
@@ -0,0 +1,31 @@
+// Exercícios de interpretação de código
+
+// 1.  a. false
+//     b. false
+//     c. true
+//     e. boolean
+
+// 2.  a. undefined
+//     b. null
+//     c. 11
+//     d. 3
+//     e. ?
+//     f. 9
+
+// Exercícios de escrita de código
+
+// 1.
+// const idade1 = prompt("Qual sua idade?")
+// const idade2 = prompt("Qual idade do seu melhor amigo(a)?")
+
+// resposta1 = idade1 > idade2
+// resposta2 = number(idade1) - number(idade2)
+
+// console.log("Sua idade é maior que do seu amigo(a)? ", resposta1)
+// console.log("A diferença de idade entre vocês é de ",resposta2 )
+
+// 2.
+// let par = prompt("Insira um numero par")
+// resposta1 = number(par)%2
+// console.log(resposta1)
+
