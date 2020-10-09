6e7f8a546ae72e56c5468ecb585000120f6c1160
IuriLami <iuri.lami@gmail.com>
2020-10-09 14:06:42 -0300

    exer 01

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,5 @@
+// Exercícios de interpretação de código
+
+// 1. A constante meuDinheiro recebe o valor em dolar, o valor vai para dentro da função conversorDeMoeda, onde pedem o input da cotação, e multiplica input*valor(dolar), retornando valor em reais, e sendo impresso pela ulima linha no console.log 
+
+// 2. 
