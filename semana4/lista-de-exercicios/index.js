77d5005b2c7df6e585f6bca64d6dc7497e7fe7c9
IuriLami <iuri.lami@gmail.com>
2020-10-09 14:14:39 -0300

    Exer 02

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -2,4 +2,5 @@
 
 // 1. A constante meuDinheiro recebe o valor em dolar, o valor vai para dentro da função conversorDeMoeda, onde pedem o input da cotação, e multiplica input*valor(dolar), retornando valor em reais, e sendo impresso pela ulima linha no console.log 
 
-// 2. 
+// 2. a constante novoMontante e segundoMontante recebem dois parametros, o primeiro sendo o tipoDeInvestimento e o segundo o valor, as informaçoes vão para o função, que seara por tipo de inestimento utilizando swith, retornando valores diferentes para cada tipo, retornando valorAposInvestimento e sendo impresso com console.log(constantes).
+
