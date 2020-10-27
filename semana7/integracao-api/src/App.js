9faa5d1eb329007988d7f2d83f3212cb9e827b93
IuriLami <iuri.lami@gmail.com>
2020-10-27 16:56:46 -0300

    tarefa incompleta

---
+++ semana7/integracao-api/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Oie</p>
+    </div>
+  );
+}
+
+export default App;
