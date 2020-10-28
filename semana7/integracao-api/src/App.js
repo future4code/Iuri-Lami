e90a0e326ba729e069960d15a1490c2a6e66a08c
IuriLami <iuri.lami@gmail.com>
2020-10-28 17:02:41 -0300

    integrando o api

--- semana7/integracao-api/src/App.js
+++ semana7/integracao-api/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <p>Oie</p>
-    </div>
-  );
-}
+// App react
+class App extends React.Component {
+  // variaveis que irei mudar
+  state= {
+    form: true
+  };
 
-export default App;
+  // mudança de pagina com renderização condicional
+  changePage = () => {
+    // estado recebe novo valor : oposto ao proprio valor
+    this.setState({form: !this.state.form});
+  };
+  
+  render() {
+    // função retorna Form para true e Users para false
+    const page = this.state.form ? <Form /> : <Users />;
+    // retorno para o usuario (o que ele enxerga)
+    return(
+      <div className ="App">
+        {page}
+        <button onClick = {this.changePage}>Mudar de Página</button>
+      </div>
+
+    );
+  }
+}
